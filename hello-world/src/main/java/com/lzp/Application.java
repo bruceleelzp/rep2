21058ac2dd3c3ac4ae0c6c22ff7adfcfdf59e2e2
bruceleelzp <bruceleelzp@163.com>
2021-11-28 22:40:31 +0800

    第一次修改

--- hello-world/src/main/java/com/lzp/Application.java
+++ hello-world/src/main/java/com/lzp/Application.java
@@ -9,8 +9,8 @@ import org.springframework.context.annotation.ComponentScan;
 @SpringBootApplication(scanBasePackages = "com")//这是一个springBoot应用
 public class Application {
     public static void main(String[] args) {
-        System.out.println("lzpfflzp");
-
+        System.out.println("lzpfflzp-lzp");
+        System.out.println("lzpfflzp-lzp");
         ConfigurableApplicationContext run = SpringApplication.run(Application.class, args);
 
 
